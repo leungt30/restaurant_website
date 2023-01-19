7c437cd2f688d5dbd60937a1de058fe8c2ed0dff
leungt30 <leungt30@mcmaster.ca>
2023-01-19 16:27:07 -0500

    newsletter added

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
-
+import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi'
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay/>
+    <Newsletter/>
   </div>
 );
 
