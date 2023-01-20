65fc689d813ee637bda7a432d7ebdcef1074d15a
leungt30 <leungt30@mcmaster.ca>
2023-01-20 15:19:36 -0500

    completed footer

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -5,7 +5,7 @@ const FindUs = () => (
   <div className='app__bg app__wrapper section__padding' id='contact'>
     <div className='app__wrapper_info'>
       <SubHeading title="Contact"/>
-      <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}>FindUs</h1>
+      <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}>Find Us</h1>
       <div className='app__wrapper-content'>
         <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
         <p className='p__cormorant' style={{color: "#DCCA87", margin:"2rem 0"}}>Opening Hours</p>
